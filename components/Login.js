280811efb029132e359ff976d0a017e6beb66765
Maria Corella <mfch98@hotmail.com>
2020-07-09 19:18:14 -0700

    object update

--- components/Login.js
+++ components/Login.js
@@ -32,7 +32,7 @@ function Login()
             }
             else
             {
-                var user = {login:res.login,id:res._id}
+                var user = {login:res.Login,id:res._id}
                 localStorage.setItem('user_data', JSON.stringify(user));
 
                 setMessage('');
